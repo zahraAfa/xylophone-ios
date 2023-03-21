1be35ae9e0fe53a10ad850041f3f6cb02b48f6cd
Anisa Zahra <anisafatimahazzahra@gmail.com>
2023-03-21 12:22:01 +0700

    final

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        sender.alpha = 0.5
+//
+//        //Code should execute after 0.2 second delay.
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+//              sender.alpha = 1.0
+//          }
+        
+        UIView.animate(withDuration: 0.3) {
+                sender.alpha = 0.5
+        }
 
-
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0
+            }
+        }
+        
+        playSound(key: sender.titleLabel?.text)
+    }
+    
+    func playSound(key: String?){
+        
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
+    }
+    
     
 
 }
